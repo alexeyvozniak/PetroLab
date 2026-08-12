55f179cbe265da373c90219d638ee1fd952efdb1
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-12 17:10:05 +0300

    Export Sources page renderer

--- petrolab/ui/pages/__init__.py
+++ petrolab/ui/pages/__init__.py
@@ -2,5 +2,6 @@
 
 from .home import render_home_page
 from .projects import render_projects_page
+from .sources import render_sources_page
 
-__all__ = ["render_home_page", "render_projects_page"]
+__all__ = ["render_home_page", "render_projects_page", "render_sources_page"]
