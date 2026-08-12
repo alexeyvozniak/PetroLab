3d7658f0acb613059a10254e43032c6f2f61eeb7
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-12 16:21:39 +0300

    Install mineral runtime compatibility fixes

--- petrolab/minerals/__init__.py
+++ petrolab/minerals/__init__.py
@@ -1,4 +1,7 @@
 from .formulae import FormulaMethod, calculate_formula, methods_for
 from .registry import MINERALS
+from .runtime_fixes import install_runtime_fixes
+
+install_runtime_fixes()
 
 __all__ = ["FormulaMethod", "calculate_formula", "methods_for", "MINERALS"]
