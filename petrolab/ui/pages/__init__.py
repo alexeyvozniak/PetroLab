2149051b9a2f2d1584b26ed8b3c06c14c530d196
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-12 17:34:27 +0300

    Export unified analyses page

--- petrolab/ui/pages/__init__.py
+++ petrolab/ui/pages/__init__.py
@@ -1,7 +1,13 @@
 """Streamlit page renderers."""
 
+from .analyses import render_analyses_page
 from .home import render_home_page
 from .projects import render_projects_page
 from .sources import render_sources_page
 
-__all__ = ["render_home_page", "render_projects_page", "render_sources_page"]
+__all__ = [
+    "render_analyses_page",
+    "render_home_page",
+    "render_projects_page",
+    "render_sources_page",
+]
