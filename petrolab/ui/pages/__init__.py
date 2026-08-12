5a96a375bfd2ae9663d01916440c14b22eea3033
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-12 20:33:56 +0300

    Expose extracted workspace pages

--- petrolab/ui/pages/__init__.py
+++ petrolab/ui/pages/__init__.py
@@ -1,17 +1,25 @@
 """Streamlit page renderers."""
 
 from .analyses import render_analyses_page
+from .change_log import render_change_log_page
+from .export import render_export_page
 from .formulae import render_formulae_page
 from .home import render_home_page
 from .images import render_images_page
+from .minerals import render_minerals_page
+from .plots import render_plots_page
 from .projects import render_projects_page
 from .sources import render_sources_page
 
 __all__ = [
     "render_analyses_page",
+    "render_change_log_page",
+    "render_export_page",
     "render_formulae_page",
     "render_home_page",
     "render_images_page",
+    "render_minerals_page",
+    "render_plots_page",
     "render_projects_page",
     "render_sources_page",
 ]
