624e536df939ad0cb18858493f6bc01bc805386c
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-13 21:13:42 +0300

    Use dashboard renderers for analyses and XY workspaces

--- petrolab/ui/pages/__init__.py
+++ petrolab/ui/pages/__init__.py
@@ -1,6 +1,6 @@
 """Streamlit page renderers."""
 
-from .analyses import render_analyses_page
+from .analyses_dashboard import render_analyses_dashboard_page as render_analyses_page
 from .article_tables import render_article_tables_page
 from .change_log import render_change_log_page
 from .export import render_export_page
@@ -9,7 +9,7 @@ from .help import render_help_page
 from .home_dashboard import render_home_dashboard_page as render_home_page
 from .images_dashboard import render_images_dashboard_page as render_images_page
 from .minerals import render_minerals_page
-from .plots import render_plots_page
+from .plots_dashboard import render_plots_dashboard_page as render_plots_page
 from .projects import render_projects_page
 from .rocks import render_rocks_page
 from .science_plots import render_science_plots_page
