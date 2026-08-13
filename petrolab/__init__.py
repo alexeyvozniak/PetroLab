0c0cf7d5f56b91a3d6031894646e7e5c3a022f65
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-13 18:50:25 +0300

    Install import and measurement runtime policies

--- petrolab/__init__.py
+++ petrolab/__init__.py
@@ -1,6 +1,8 @@
 __version__ = "0.10.2"
 
 from .measurement_policy import install as _install_measurement_policy
+from .services.import_runtime import install as _install_import_runtime
 
 _install_measurement_policy()
-del _install_measurement_policy
+_install_import_runtime()
+del _install_measurement_policy, _install_import_runtime
