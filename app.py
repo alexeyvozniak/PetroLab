b613837450fc3b0e0986b93b26f56f2ba8992f19
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-13 23:37:14 +0300

    Avoid repeated plot policy wrapping on Streamlit reruns

--- app.py
+++ app.py
@@ -30,13 +30,18 @@ from petrolab.ui.pages import (
     render_ternary_page,
     render_updates_page,
 )
+from petrolab.ui.pages import plots as legacy_plots
 from petrolab.ui.plot_page_policy import install as install_plot_page_policy
 from petrolab.ui.theme import apply_theme
 
 
 st.set_page_config(page_title="ПетроЛаб", page_icon="◈", layout="wide")
 install_import_page_policy()
-install_plot_page_policy()
+if not getattr(legacy_plots, "_petrolab_plot_policy_installed", False):
+    install_plot_page_policy()
+    legacy_plots._petrolab_plot_policy_installed = True
+else:
+    legacy_plots._petrolab_workspace_call_index = 0
 ensure_storage()
 settings = load_settings()
 apply_theme(str(settings.get("ui_density", "comfortable")))
