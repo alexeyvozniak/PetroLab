035a47a95c3e6d87e0367e998bd83834674e6f7e
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-13 19:05:49 +0300

    Install whole-rock integrity runtime

--- petrolab/__init__.py
+++ petrolab/__init__.py
@@ -1,16 +1,17 @@
 __version__ = "0.10.2"
 
 from .measurement_policy import install as _install_measurement_policy
-
 _install_measurement_policy()
 del _install_measurement_policy
 
 from .services.import_runtime import install as _install_import_runtime
-
 _install_import_runtime()
 del _install_import_runtime
 
 from .minerals.formula_policy import install as _install_formula_policy
-
 _install_formula_policy()
 del _install_formula_policy
+
+from .rock_runtime import install as _install_rock_runtime
+_install_rock_runtime()
+del _install_rock_runtime
