3ba337ee78f605279fd9c97c5d7a66012410528a
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-14 12:07:31 +0300

    Expose mixed-mineral review page

--- petrolab/ui/pages/__init__.py
+++ petrolab/ui/pages/__init__.py
@@ -11,6 +11,7 @@ from .help import render_help_page
 from .home_dashboard import render_home_dashboard_page as render_home_page
 from .images_dashboard import render_images_dashboard_page as render_images_page
 from .minerals import render_minerals_page
+from .mixed_minerals import render_mixed_minerals_page
 from .plots_dashboard import render_plots_dashboard_page as render_plots_page
 from .projects import render_projects_page
 from .rocks import render_rocks_page
@@ -24,7 +25,7 @@ from .updates import render_updates_page
 __all__ = [
     "render_analyses_page", "render_article_tables_page", "render_change_log_page",
     "render_data_intake_page", "render_database_browser_page", "render_export_page", "render_formulae_page",
-    "render_help_page", "render_home_page", "render_images_page", "render_minerals_page",
+    "render_help_page", "render_home_page", "render_images_page", "render_minerals_page", "render_mixed_minerals_page",
     "render_plots_page", "render_projects_page", "render_rocks_page", "render_science_plots_page",
     "render_settings_page", "render_sources_page", "render_statistics_page",
     "render_ternary_page", "render_updates_page",
