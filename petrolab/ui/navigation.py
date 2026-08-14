c975514233f47eb3a5cda4022f95dc35038bde94
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-14 16:41:52 +0300

    Add equilibrium navigation

--- petrolab/ui/navigation.py
+++ petrolab/ui/navigation.py
@@ -17,7 +17,7 @@ NAV_SECTIONS = {
     "Исследование": [
         ("plots", "XY-диаграммы"), ("ternary", "Треугольные"),
         ("science_plots", "Научные диаграммы"), ("statistics", "Статистика"),
-        ("generations", "Поколения"), ("thermobarometry", "Термобарометрия"),
+        ("generations", "Поколения"), ("equilibrium", "Равновесные пары"), ("thermobarometry", "Термобарометрия"),
     ],
     "Материалы": [
         ("rocks", "Породы"), ("slides", "Шлифы и поля"), ("images", "Изображения"),
