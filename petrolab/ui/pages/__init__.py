0882399b63e1eb673550c256f4de6f1fa7d02e97
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-15 13:20:46 +0300

    Expose composite multi panel and whole rock pages

--- petrolab/ui/pages/__init__.py
+++ petrolab/ui/pages/__init__.py
@@ -8,6 +8,7 @@ from .attention import render_attention_page
 from .batch_edit import render_batch_edit_page
 from .change_log import render_change_log_page
 from .collaboration import render_collaboration_page
+from .composite_points import render_composite_points_page
 from .data_intake import render_data_intake_page
 from .database_browser import render_database_browser_page
 from .distribution import render_distribution_page
@@ -23,6 +24,7 @@ from .images_dashboard import render_images_dashboard_page as render_images_page
 from .measurements import render_measurements_page
 from .minerals import render_minerals_page
 from .mixed_minerals import render_mixed_minerals_page
+from .multi_panel import render_multi_panel_page
 from .object_workspace import render_object_workspace_page
 from .plots_dashboard import render_plots_dashboard_page as render_plots_page
 from .projects import render_projects_page
@@ -38,20 +40,22 @@ from .ternary import render_ternary_page
 from .thermobarometry import render_thermobarometry_page
 from .thin_section_workspace import render_thin_section_workspace_page
 from .updates import render_updates_page
+from .whole_rock_compare import render_whole_rock_compare_page
 
 __all__ = [
     "render_add_data_page", "render_analyses_page", "render_analytical_sessions_page",
     "render_article_tables_page", "render_attention_page", "render_batch_edit_page",
     "render_calculate_page", "render_change_log_page", "render_collaboration_page",
-    "render_compare_page", "render_data_intake_page", "render_database_browser_page",
-    "render_distribution_page", "render_equilibrium_page", "render_export_page",
-    "render_formulae_page", "render_generations_page", "render_global_search_page",
-    "render_guided_workflow_page", "render_help_page", "render_home_page",
-    "render_images_page", "render_measurements_page", "render_minerals_page",
-    "render_mixed_minerals_page", "render_object_workspace_page", "render_plots_page",
-    "render_projects_page", "render_publish_page", "render_quick_import_page",
-    "render_rocks_page", "render_science_plots_page", "render_settings_page",
-    "render_slides_page", "render_sources_page", "render_statistics_page",
-    "render_ternary_page", "render_thermobarometry_page", "render_thin_section_workspace_page",
-    "render_updates_page",
+    "render_compare_page", "render_composite_points_page", "render_data_intake_page",
+    "render_database_browser_page", "render_distribution_page", "render_equilibrium_page",
+    "render_export_page", "render_formulae_page", "render_generations_page",
+    "render_global_search_page", "render_guided_workflow_page", "render_help_page",
+    "render_home_page", "render_images_page", "render_measurements_page",
+    "render_minerals_page", "render_mixed_minerals_page", "render_multi_panel_page",
+    "render_object_workspace_page", "render_plots_page", "render_projects_page",
+    "render_publish_page", "render_quick_import_page", "render_rocks_page",
+    "render_science_plots_page", "render_settings_page", "render_slides_page",
+    "render_sources_page", "render_statistics_page", "render_ternary_page",
+    "render_thermobarometry_page", "render_thin_section_workspace_page",
+    "render_updates_page", "render_whole_rock_compare_page",
 ]
