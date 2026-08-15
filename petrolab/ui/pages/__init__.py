650a9cc2151cf36e7cd860ae93db902b70ac917a
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-15 16:13:41 +0300

    Expose grain profile page

--- petrolab/ui/pages/__init__.py
+++ petrolab/ui/pages/__init__.py
@@ -17,6 +17,7 @@ from .export import render_export_page
 from .formulae import render_formulae_page
 from .generations import render_generations_page
 from .global_search import render_global_search_page
+from .grain_profile import render_grain_profile_page
 from .guided_workflow import render_guided_workflow_page
 from .help import render_help_page
 from .home_dashboard import render_home_dashboard_page as render_home_page
@@ -61,8 +62,8 @@ __all__ = [
     "render_compare_page", "render_composite_points_page", "render_data_intake_page",
     "render_database_browser_page", "render_distribution_page", "render_equilibrium_page",
     "render_export_page", "render_formulae_page", "render_generations_page",
-    "render_global_search_page", "render_guided_workflow_page", "render_help_page",
-    "render_home_page", "render_images_page", "render_measurements_page",
+    "render_global_search_page", "render_grain_profile_page", "render_guided_workflow_page",
+    "render_help_page", "render_home_page", "render_images_page", "render_measurements_page",
     "render_minerals_page", "render_mixed_minerals_page", "render_multi_panel_page",
     "render_object_workspace_page", "render_plots_page", "render_projects_page",
     "render_publish_page", "render_quick_import_page", "render_rocks_page",
